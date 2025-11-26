1bf2bd4d2f7179e1984b44082fc4a2843a63ea38
BharathKumaravel <bharathkumaravel27@gmail.com>
2025-11-26 12:46:10 +0530

    Initial commit of old project

---
+++ DataType.java
@@ -0,0 +1,14 @@
+package JAVA_PROBLEMS;
+
+public class DataType {
+    static void main() {
+        int a=10;
+        char c='b';
+        String str="Hello world";
+        System.out.println("a="+a);
+        System.out.println("c="+c);
+        System.out.println("str="+str);
+    }
+}
+
+
