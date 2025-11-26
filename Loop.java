05f42b7f38eab8cf66dfba11fda63882d10831bf
BharathKumaravel <163282187+BharathKumaravel@users.noreply.github.com>
2025-11-26 13:11:33 +0530

    Implement loops with user input in Loop.java
    Added user input for two numbers and loops to print values.

--- Loop.java
+++ Loop.java
@@ -1,4 +1,23 @@
 package JAVA_PROBLEMS;
 
+import java.util.Scanner;
+
 public class Loop {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a number ");
+        int n=sc.nextInt();
+        System.out.println("enter a number ");
+        int m=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            System.out.println(i);
+        }
+
+        while(m<25)
+        {
+            System.out.println(m);
+            m+=1;
+        }
+    }
 }
